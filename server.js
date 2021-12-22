704a7a556220b6f96e74a4d0e94ed86d54ba39d3
Football Worker <contacttofw@gmail.com>
2021-12-23 02:30:32 +0300

    change server address4

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import http from 'http'
 import app from "./express.js";
 import socket from "./controllers/socket.controller.js";
 
@@ -14,11 +14,15 @@ mongoose.connection.on("error", () => {
   throw new Error(`unable to connect to database: ${process.env.MONGODB_LINK}`);
 });
 
-const server = app.listen(process.env.PORT, (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.info("Server started on port %s.", process.env.PORT);
-});
+// const server = app.listen(process.env.PORT, (err) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.info("Server started on port %s.", process.env.PORT);
+// });
+
+const server = http.createServer(app);
 
 socket(server);
+
+server.listen(process.env.PORT, () => console.log(`Server has started.`));
