ef826461bd720932ef8bfc8eadce4fa8cbd0e71b
Football Worker <contacttofw@gmail.com>
2021-12-22 23:13:56 +0300

    checkSocket

--- controllers/socket.controller.js
+++ controllers/socket.controller.js
@@ -3,16 +3,19 @@ import { Server } from "socket.io";
 import Chat from "./../models/chat.model.js";
 
 export default (server) => {
-  const io = new Server(server, { cors: process.env.CLIENTURI });
+  const io = new Server(server, { cors: "ws://footballworker.net" });
 
   io.on("connection", function (socket) {
     socket.on("join chat room", (data) => {
+      console.log('User Joined.');
       socket.join(data.room);
     });
     socket.on("new message", (data) => {
+      console.log("Message sent")
       backFunc(data.messageInfo, data.room);
     });
     socket.on("leave chat room", (data) => {
+      console.log('User Left.')
       socket.leave(data.room);
     });
   });
